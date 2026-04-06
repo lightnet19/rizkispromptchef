765e36af8bca762466dc522eb3bab61a57e79670
lightnet19 <light.net19@gmail.com>
2026-04-06 16:38:55 +0700

    Create presets.js

---
+++ presets.js
@@ -0,0 +1,28 @@
+const PRESETS = {
+  roles: [
+    "Mentor Cerdas yang Suka Bercanda",
+    "Profesor Akademik yang Formal",
+    "Konsultan Senior yang Tegas",
+    "Teman Belajar yang Santai",
+    "Kritikus Objektif"
+  ],
+  formats: [
+    "Langkah demi langkah",
+    "Poin-poin (bullet)",
+    "Esai singkat",
+    "Tabel perbandingan",
+    "Dialog imajinatif"
+  ],
+  tones: [
+    "Formal",
+    "Santai",
+    "Profesional",
+    "Persuasif",
+    "Humoris"
+  ],
+  defaults: {
+    role: "Mentor Cerdas yang Suka Bercanda",
+    format: "Langkah demi langkah",
+    tone: "Profesional"
+  }
+};
